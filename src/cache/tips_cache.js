18106e2282e6eb6af39587b512824d0aa97e8a3b
Anastas Fonotov <anastasf@gmail.com>
2016-06-01 15:05:45 +0300

    update tips cache

--- src/cache/tips_cache.js
+++ src/cache/tips_cache.js
@@ -14,7 +14,10 @@
         function (pipEnums, pipDataCache) {
 
             return {
-                readTips: readTips
+                readTips: readTips,
+                onTipCreate: onTipCreate,
+                onTipUpdate: onTipUpdate,
+                onTipDelete: onTipDelete                
             };
 
             function readTips(params, successCallback, errorCallback) {
@@ -24,6 +27,25 @@
 
                 return pipDataCache.retrieveOrLoad(params, successCallback, errorCallback);
             };
+            
+            function onTipCreate(params, successCallback) {
+                return pipDataCache.addDecorator(
+                    'tips', params,
+                    pipTagsCache.tagsUpdateDecorator(params, successCallback)
+                );
+            };
+
+            function onTipUpdate(params, successCallback) {
+                return pipDataCache.updateDecorator(
+                    'tips', params,
+                    pipTagsCache.tagsUpdateDecorator(params, successCallback)
+                );
+            };
+
+            function onTipDelete(params, successCallback) {
+                return pipDataCache.removeDecorator('tips', params, successCallback);
+            };
+                        
         }
     );
 
